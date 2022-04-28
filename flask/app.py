378ad11c4e44621955e730986ca427ff8ba4a42f
pavanreddy2693 <pavanreddy2693@gmail.com>
2022-04-28 08:50:47 +0530

    docker learnings

---
+++ flask/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def AwesomeContainers():
+    return 'This is healthy flask application.'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
